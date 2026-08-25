0c37aec373f74d15619cb73e29ba5312bb9bac8e
Tejas Kumar <mac@Macs-MacBook-Pro.local>
2026-08-25 11:31:52 +0530

    Initialize rate limiting API gateway

---
+++ src/test/java/com/example/RateLimitingAPIGateway/RateLimitingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RateLimitingAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
